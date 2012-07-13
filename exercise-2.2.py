e83a88e7d67e84bab092d7d7b4ecba97157aa36e
Terry Bates <terryjbates@gmail.com>
2012-07-13 00:56:35 -0400

    Completed Exercise 2.2

---
+++ exercise-2.2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-2.2.py
+
+A script to modify exercise expressions into print statements.
+
+Created by Terry Bates on 2012-07-13.
+Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	print 17
+	x=5
+	print x
+	print x+1
+
+
+if __name__ == '__main__':
+	main()
+
