c7dffa6a4ab8f8f5e2a4c612a52b6332476996a8
Terry Bates <terryjbates@gmail.com>
2012-08-01 23:05:58 -0400

    Created entire file in one shot. Function called is_triangle() and prompt_user(). Re-used the prompt code from previous exercises then called the is_triangle() function. As per book instructions, did convert the input values to integers. Meaning you can fake out by entering floating numbers with "blah.99999" and still have it says "it is a triangle."

---
+++ chapter-5/exercise-5.14.2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-5.14.2.py
+
+Created by Terry Bates on 2012-08-01.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+def is_triangle(a,b,c):
+    if (a + b > c) and (a + c > b) and (b + c > a):
+        print "Yes"
+    else:
+        print "No"
+
+def prompt_user():
+    prompt = "Please enter side "
+    for value in ['a', 'b', 'c']:
+        new_prompt = prompt + value +': ' # modify prompt for value in loop
+        my_input = raw_input(new_prompt)   # get input
+        float_input = float(my_input)      # convert the entered value to float
+        exec ("%s=%f" % (value, float_input))
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    is_triangle(a,b,c)
+
+def main():
+    # is_triangle(5,3,4)
+    # is_triangle(5,3,12)
+    prompt_user()
+
+if __name__ == '__main__':
+    main()
+
