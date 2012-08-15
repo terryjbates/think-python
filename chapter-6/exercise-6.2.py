dcb77df6e8477b3116f5102aea0852a5d6977b0c
Terry Bates <terryjbates@gmail.com>
2012-08-14 22:48:56 -0400

    We totally remove the import os, import sys statement, only have a single print statement calling our 'hypotenuse' function.

--- chapter-6/exercise-6.2.py
+++ chapter-6/exercise-6.2.py
@@ -11,15 +11,12 @@ Created by Terry Bates on 2012-08-14.
 Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
 """
 
-import sys
-import os
 import math
 
 def hypotenuse(x,y):
-    print math.sqrt(x**2 + y**2)
     return math.sqrt(x**2 + y**2)
 
 
 if __name__ == '__main__':
-    hypotenuse(3,4)
+    print "hypotenuse is " + str(hypotenuse(3,4))
 
