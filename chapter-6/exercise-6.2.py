e4dd64c8a9bb9836ed09d8f3a3aabde35bfca09a
Terry Bates <terryjbates@gmail.com>
2012-08-14 22:31:34 -0400

    Initial check-in of 'hypotenuse' script, Exercise 6.2

---
+++ chapter-6/exercise-6.2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-6.2.py
+
+Script to incrementally develop 'hypotenuse' function. Return length of hypotenuse of right triangle,
+give length of two legs as an argument. Record each step as you go.
+
+
+Created by Terry Bates on 2012-08-14.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def hypotenuse():
+    pass
+
+
+if __name__ == '__main__':
+    hypotenuse(2,3)
+
