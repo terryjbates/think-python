6835ccd27d6fa627553a686f8034bbb70a27997f
Terry Bates <terryjbates@gmail.com>
2012-08-14 22:20:02 -0400

    Completed 'compare' function. Return '1' if first arg > second. Return '0' if first arg == second arg. Fudged the third and said return '-1' for any other case.

---
+++ chapter-6/exercise-6.1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-6.1.py
+
+Write a compare function.
+Return 1 if x > y
+Return 0 if x == y
+Return -1 if x < y
+
+Created by Terry Bates on 2012-08-14.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def compare(x,y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    else: # Implicit
+        return -1
+        
+
+if __name__ == '__main__':
+    print compare(5,6)
+    print compare(3,2)
+    print compare(2,2)
