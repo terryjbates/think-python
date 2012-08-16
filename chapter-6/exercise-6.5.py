19a4c286093001840b0c41d410acfdf0c0d7d624
Terry Bates <terryjbates@gmail.com>
2012-08-15 22:51:56 -0400

    Created first (and maybe last) version of Exercise 6.5. This demonstrates the Ackermann function. It appears that if values greater than example (3,4) will definitely make you hit the recursion limit. May be a good exercise to actually trace this out.

---
+++ chapter-6/exercise-6.5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-6.5.py
+
+Implementation of the Ackermann function.
+
+Appears that ack(3,4) ends easily at 125, going to (4,4) seems to run endlessly. We *definitely* hit recursion depth:
+
+import cProfile
+s = 'ack(3,4)'
+cProfile.run(s)
+...
+Runtime Error Maximum recursion depth exceeded.
+
+Created by Terry Bates on 2012-08-15.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def ack(m,n):
+    if m == 0:
+        return n + 1
+    elif m > 0: # we use DRY
+        if n == 0:
+            return ack(m-1,1) 
+        elif n > 0:
+            return ack(m-1, ack(m,n-1))
+
+
+
+if __name__ == '__main__':
+    print ack(3,4)
+    print ack(4,4)
+
