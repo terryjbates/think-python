bb7d9ea42e0d133bc31f7151219d8ed53b230e61
Terry Bates <terryjbates@gmail.com>
2012-08-16 22:05:47 -0400

    Created two versions of is_palindrome(). One commented, the other not. The one I opted for uses a 'guardian,' figuring easy to tell from the bat whether or not the word is a palindrome or not. middle() was interesting, since I thought, erroneously, it would grab the middle character. Instead it lops off first and last characters. Thus, you can recursively make the word smaller and smaller, till you can return.

---
+++ chapter-6/palindrome.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+palindrome.py
+
+AKA exercise-6.6. 
+
+
+Created by Terry Bates on 2012-08-15.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def first(word):
+    return word[0]
+    
+def last(word):
+    return word[-1]
+
+def middle(word):
+    return word[1:-1]
+
+def is_odd_char(word):
+    if len(word) % 2 == 1:
+        return True
+    else:
+        return False
+    
+def first_equals_last(a):
+    if first(a) == last (a):
+        return True
+    else:
+        return False
+    
+
+def is_palindrome(word):    
+    # base case is 3 character word
+    if not first_equals_last(word) or not is_odd_char(word):
+        return False
+    else:
+        if len(word) > 3:
+            word = middle(word)
+            return is_palindrome(word)
+        elif len(word) == 3:
+            return True
+        else:
+            return False
+    # First Attempt:        
+    # if first_equals_last(word) and len(word) > 3:
+    #     print word + ": in if"
+    #     word = middle(word)
+    #     return is_palindrome(word)
+    # elif first_equals_last(word) and len(word) == 3:
+    #     print word + ": in elif"
+    #     return True
+    # else:
+    #     print word + ": in else"
+    #     return False
+    # 
+if __name__ == '__main__':
+    print 'bob ' + str(is_palindrome('bob'))
+    print 'terry ' + str(is_palindrome('terry'))
+    print 'coraroc ' + str(is_palindrome('coraroc'))
