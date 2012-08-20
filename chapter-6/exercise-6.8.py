6cb6fa5a8776bf0aaeb66eefc8edc2b283440fe5
Terry Bates <terryjbates@gmail.com>
2012-08-20 00:18:31 -0400

    Completion of Exercise 6.8. Interesting that this use of Euclidean algorithm differs from Wikipedia definition slightly, since they use subtraction between values, whereas this exercise uses the remainder of division between two numbers to go to work.

---
+++ chapter-6/exercise-6.8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-6.8.py
+
+Greatest commond divisor with Euclid's algorithm. PITA to realize that we must return the function vaule, not simply invoke the gcd function via recursion.
+
+Created by Terry Bates on 2012-08-19.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+
+def gcd(a,b):
+    # catch base case from the door
+    if b == 0:
+        return a
+    # otherwise, do the other steps
+    else:
+        # compute remainder
+        remainder = a % b
+        # If we have base case, no need to
+        # call again
+        if remainder == 0:
+            return b
+        # otherwise, we recurse and grab the return value
+        else:
+            return gcd(b, remainder)
+
+
+if __name__ == '__main__':
+    print  gcd(9, 3)
+    print gcd(15, 12)
+    print gcd(24,8)
