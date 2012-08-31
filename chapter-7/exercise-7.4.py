bf5bd8a41820e13421095db139bd187c29330ca2
Terry Bates <terryjbates@gmail.com>
2012-08-30 22:18:47 -0400

    Initial commit of Exercise 7.4; create a loop that accepts user input, evaluates it with "eval," and prints the result. Banging my head on this for a while, since I kept thinking that an "expression" is the same as a "statement." I kept plugging in "i=j" and getting mad. Better to have it actually do stuff. As in:
    i = 5
    j = 2
    <start program>
    >>> i+j
    …and see the result. It is not a full on Python interpreter.

---
+++ chapter-7/exercise-7.4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-7.4.py
+
+Created by Terry Bates on 2012-08-29.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+def eval_loop():
+    while True:
+        user_string = raw_input('>>> ')
+        if user_string == 'done':
+            break
+        else:
+            print user_string
+            result = eval(user_string)
+            print result
+
+if __name__ == '__main__':
+    eval_loop()
