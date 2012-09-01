955272a44f3ad8815a28066be87a5c1ac086d0c5
Terry Bates <terryjbates@gmail.com>
2012-09-01 01:46:11 -0400

    First, and hopefully last, commit of the 'estimate_pi()' program. I kept banging my head on this since I accidentally inverted the numerator and denominator. The solution also re-implements factorial, whereas I just imported via "import math."

---
+++ chapter-7/exercise-7.5.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-7.5.py
+
+Created by Terry Bates on 2012-08-30.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import math
+
+
+def estimate_pi():
+    coefficient = 2 * math.sqrt(2) / 9801
+    k = 0
+    total = 0
+    while True:
+        den = math.factorial(k)**4.0 * (394.0**(4.0*k))
+        num = math.factorial(4.0*k) * (1103.0 + (26390.0*k))
+        interim_sum =  coefficient * (num/den) 
+        total = total + interim_sum
+        if abs(interim_sum) < 1e-15:
+            break
+        k = k + 1
+    return 1 / total
+
+
+if __name__ == '__main__':
+    print estimate_pi()
+
