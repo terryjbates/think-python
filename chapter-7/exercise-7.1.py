6950f10999fc1a354b53466dad616f24f23e860d
Terry Bates <terryjbates@gmail.com>
2012-09-05 23:14:55 -0400

    Added descriptive comment

--- chapter-7/exercise-7.1.py
+++ chapter-7/exercise-7.1.py
@@ -3,7 +3,7 @@
 """
 exercise-7.1.py
 
-Rewrite program from section 5.8 using iteration instead of recursion
+Rewrite program from section 5.8 using iteration instead of recursion.
 
 Created by Terry Bates on 2012-08-21.
 Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
