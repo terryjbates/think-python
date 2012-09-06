bf6b19a609a775f2011b65c1acdcaf30511f0996
Terry Bates <terryjbates@gmail.com>
2012-09-05 23:44:46 -0400

    Modify 'find' function to accept a third argument and use that argument as the starting index from where to begin search for the specified character.

---
+++ chapter-8/execrise-8.3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+execrise-8.3.py
+
+Created by Terry Bates on 2012-09-05.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def find(word,letter,start):
+    if start:
+        index = start
+    else:
+        index = 0
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return -1
+    
+
+
+if __name__ == '__main__':
+    print find('foo fighters rock', 'i', 6)
+    print 'foo fighters rock'[5]
