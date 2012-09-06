a0afcb7db31a7ae0b79ed94c100ce5219c1d3d8a
Terry Bates <terryjbates@gmail.com>
2012-09-05 23:20:45 -0400

    Write a function to take string as argument, display the letters backwards, one per line. This solution less convoluted than the last I attempted.

---
+++ chapter-8/execrise-8.1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+execrise-8.1.py
+
+Created by Terry Bates on 2012-09-05.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def backward_with_lines(word):
+    index = len(word)
+    while index > 0:
+        print word[index-1]
+        index = index -1
+
+if __name__ == '__main__':
+	backward_with_lines('foo')
+
