3c9202ec334beb0267bf90ddfbb5e59d15179bc5
Terry Bates <terryjbates@gmail.com>
2012-09-05 23:28:36 -0400

    Fixed function such that it will properly spell 'Quack' and 'Ouack.' Probably case they wanted use of "==" and not 'in'.

---
+++ chapter-8/execrise-8.2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+execrise-8.2.py
+
+Created by Terry Bates on 2012-09-05.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+
+
+if __name__ == '__main__':
+    prefixes = 'JKLMNOPQ'
+    suffix = 'ack'
+
+    for letter in prefixes:
+        if (letter in 'O') or (letter in 'Q'):
+            letter = letter + 'u'
+        print letter + suffix
+    
