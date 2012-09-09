c65a46d6f9b4c2d2d262bcb35d0ed8302fe6ecd8
Terry Bates <terryjbates@gmail.com>
2012-09-08 21:36:25 -0400

    Created snippet that solves question in Exercise 8.8. The key is to modify the 'if' statement so that it adjust for the index being 0. Basically, tell it to wait until j > -1. Otherwise, the match will occur with all chars except the last and first of the two words.

---
+++ chapter-8/execrise-8.8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+execrise-8.8.py
+
+Created by Terry Bates on 2012-09-08.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
