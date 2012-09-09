847e56bdc33922ae7b8e2ad99282ecad56146cbe
Terry Bates <terryjbates@gmail.com>
2012-09-09 16:42:18 -0400

    Most irritating exercise in the book. I gave up on 'chr' and 'ord'. I created base tables out of string with two copies of lowercase and uppercase letters, use the rotation number to generate a lookup table string. First find the first string in the base table. Then, once you have the index, extract that from the lookup table. Voila, it works. Doing all this stuff with 'chr' and 'ord' is made more difficult because of the gab between 'Z' and 'a'. Use the string methods you just read about in the previous exercises, or do a bunch of kung fu with if/elif/yadda. I am lazy.

--- chapter-8/exercise-8.12.py
+++ chapter-8/exercise-8.12.py
@@ -7,36 +7,41 @@ Created by Terry Bates on 2012-09-08.
 Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
 """
 
-import sys
-import os
 
 def rotate_word(word, rotate_num):
     # a = 97, z = 122, A = 65, Z = 90
     new_word = ''
-    index = 0
+    base_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy"
+    lookup_table = ''
+    
+    # Cut off the rotation number
+    # assume this is a positive number for now
+    rotate_portion = base_table[:rotate_num]
+    
+    # Run through the chars in base_table
+    # Append to the end of lookup_table
+    for char in base_table[rotate_num:]:
+        # populate the lookup_table, starting at the rotated number
+        lookup_table = lookup_table + char
+    # ...then append rotated portion to the tail end    
+    lookup_table = lookup_table + rotate_portion
+
     for char in word:
-        # Get the ordinal value
-        my_ord = ord(char)
-        print "original ord value" , my_ord        
-        # Add the rotation to ordinal value
-        # If this is negative, that is OK
-        # since adding a negative number = subtraction
-        my_ord = my_ord + rotate_num
-        print "new ord value" , my_ord
-        #print chr(my_ord)
-        if my_ord > 90 and my_ord < 97 :
-            my_ord = my_ord + 7
-        elif my_ord > 122:
-            my_ord = my_ord - 57
-        elif my_ord < 65:
-            my_ord = my_ord + 57
-        print "ord value after if/elif" , my_ord        
-        my_char = chr(my_ord)
-        print "old char: %s | new char: %s \n" % (char.encode(),my_char.encode())
-        new_word = new_word + my_char
-        index = index + 1    
+        # if it is alphanumeric
+        if char.isalnum():
+            # Find the first instance of the char in the base_table
+            index = base_table.find(char,1)
+            # then lookup up this index in the lookup_table
+            new_word = new_word + lookup_table[index]
+        # else, keep the character as it is.
+        else:
+            new_word = new_word + char
     return new_word
 
 if __name__ == '__main__':
-    print rotate_word('banana', 13)
-    print rotate_word("In the elevators, the extrovert looks at the OTHER guy's shoes.", 13)
+    #print rotate_word('banana', 13)
+    print "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    print rotate_word("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 13)
+    print
+    print "Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf."
+    print rotate_word("Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.", 13)
