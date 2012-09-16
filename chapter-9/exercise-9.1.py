ca5a046628e34d874a20e310dfb969ac958195bc
Terry Bates <terryjbates@gmail.com>
2012-09-15 23:04:16 -0400

    Completed Exercise 9.1. Had to manually download the 'words.txt' file from: http://swampy.googlecode.com/svn-history/r3/branches/experiment.1/python2/words.txt Added to the 'swampy' egg directory, then referenced directly in script so that "open" could work as expected. May be case that this came with "swampy" if downloaded. I used "easy_install".

---
+++ chapter-9/exercise-9.1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-9.1.py
+
+Program to read in the swampy module's 'words.txt' and only print words that are greater than 20 chars.
+
+Created by Terry Bates on 2012-09-15.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import swampy
+
+# Neat trick learned from 
+from os import path
+#swampy_dir = path.join(path.dirname(__file__), 'swampy')
+swampy_dir = "/Library/Python/2.7/site-packages/swampy-2.1.1-py2.7.egg/swampy/"
+
+
+def main():
+    #create a variable called 'words_txt', make it have path of words.txt
+    words_txt = swampy_dir + '/words.txt'
+    print words_txt
+    # open the file
+    f = open(words_txt)
+    # Do a loop, if the line is > 20 chars, print
+    for line in f:
+        if len(line) > 20:
+            # we strip the line, or else it will print the trailing '\r\n'
+            print line.strip()
+            
+    
+if __name__ == '__main__':
+	main()
+
