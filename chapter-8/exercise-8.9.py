72a6000b3da85ae130ba69131d58ec16d2ab69cb
Terry Bates <terryjbates@gmail.com>
2012-09-20 07:57:52 -0400

    Completed one-line implementation of the 'is_palindrome' exercise. Took me a second to figure out you can do it one line, since have been in mental state of using if/else for readability.

---
+++ chapter-8/exercise-8.9.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-8.9.py
+
+Created by Terry Bates on 2012-09-08.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+def is_palindrome(word):
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+        
+if __name__ == '__main__':
+    print is_palindrome('bob')
+    print is_palindrome('alpapla')
