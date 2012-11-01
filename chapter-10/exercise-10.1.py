1c8f0213910d234bae5d89befb729530ebd5a4fe
Terry Bates <terryjbates@gmail.com>
2012-10-31 23:50:35 -0400

    Completed Exercise 10.1. Create a program that has a function that will take a list of numbers and return a cumulative list. The ith element will be cumulative list of that element and all elements previous to it. So:
    [1,2,3] --> [1, 3, 6]

---
+++ chapter-10/exercise-10.1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-10.1.py
+
+Create a function that will take a list [1,2,3]
+and return a list such that i = i-1 + 1 so:
+[1,2,3] --> [1, 3, 6]
+Created by Terry Bates on 2012-10-31.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+def cum_sum(start_list):
+    my_sum = 0
+    sum_list = []
+    for i in range(len(start_list)):
+        my_sum += start_list[i]
+        # print my_sum
+        # the original idea of sum_list[i] = my_sum *does not work*
+        sum_list.append(my_sum)
+    return sum_list
+
+def main():
+    start_list = [1,2,3]
+    print cum_sum(start_list)
+
+if __name__ == '__main__':
+	main()
+
