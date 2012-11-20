5702776e67b900408c3d800b97df8ed41f9d0871
Terry Bates <terryjbates@gmail.com>
2012-11-19 22:32:10 -0500

    Completed Exercise 10.3. Create a 'is_sorted' function to return 'True' if a list is sorted and 'False' if not.

---
+++ chapter-10/exercise-10.3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-10-3.py
+
+Write a function called 'is_sorted'. List as parameter. Return 'True' if elements are sorted, 'False' otherwise. You can use "<,> operates"
+
+Created by Terry Bates on 2012-11-19.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+
+def is_sorted(my_list):
+    index = 0 
+    list_length = len(my_list)
+    
+    while index < list_length-1 :
+        current = my_list[index]
+        next = my_list[index+1]
+        if next >= current:
+            print "%s is bigger than %s" % (next, current)
+            index += 1
+        else: 
+            return False
+    return True
+
+def main():
+    test_list = [1, 2, 2, 3, 4]
+    print is_sorted(test_list)
+
+
+if __name__ == '__main__':
+    main()
+
