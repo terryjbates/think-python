d03360667679efebde8ba8c367b422cb0253d298
Terry Bates <terryjbates@gmail.com>
2012-11-20 21:47:23 -0500

    Completed Exercise 10.4. Create a function called 'is_anagram' to take two strings and returns 'True' if the strings are anagrams. This takes the strings, makes them lists, uses the sort() function to make lists sorted, in-place. Then compare the lists. If the are anagrams, the sorted lists should be equivalent.

---
+++ chapter-10/exercise-10.4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-10.4.py
+
+Write a function 'is_anagram' that takes two strings and returns true if they are anagrams
+
+Created by Terry Bates on 2012-11-19.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+def is_anagram(first_string, second_string):
+    # Create lists from the strings passed in as params
+    first_list  = list(first_string)
+    second_list = list(second_string)
+
+    # Sort the lists. These are mutable, so list contents will be altered.
+    first_list.sort()
+    second_list.sort()
+    if first_list == second_list:
+        return True
+    else:
+        return False
+
+def main():
+    string_a ='foobar'
+    string_b ='roobfa'
+    print is_anagram(string_a, string_b)
+    
+if __name__ == '__main__':
+    main()
+
