4b5f2a8e235165365c9bb776750f300f4dd55a78
Terry Bates <terryjbates@gmail.com>
2013-01-03 07:07:32 -0500

    Changed ordering of functions, so that "bisect" is defined first. Added an "is_even_num" function to allow for toggling between the original word and the search word, as we seek to generate a new interlocked word.

--- chapter-10/exercise-10.10.py
+++ chapter-10/exercise-10.10.py
@@ -13,20 +13,6 @@ import time
 
 swampy_dir = "/Library/Python/2.7/site-packages/swampy-2.1.1-py2.7.egg/swampy/"
 
-def shuffle_word(plain_word, search_word):
-    plain_word_list = []
-    search_word_list = []
-    # Make the words into list, then get all even or odd list members
-    
-
-
-def interlock(word, total_word_list):
-    for search_word in total_word_list:
-        new_word = shuffle_word(word, search_word)
-        
-    if bisect(new_word, total_word_list):
-        print new_word
-
 def bisect(search_term, my_list):
     # Shamelessly stolen from bisect.py
     lo = 0
@@ -53,7 +39,46 @@ def append_word_to_list(my_list, file_handle, method):
         for line in file_handle:
             line = line.strip()
             my_list = my_list + [line]
-    
+
+def is_even_num(number):
+    if number % 2 == 0 or number == 0:
+        return True
+    else:
+        return False
+
+
+def shuffle_word(plain_word, search_word):
+    # Get two lists from the words we are given
+    plain_word_list = list(plain_word)
+    search_word_list = list(search_word)
+    shuffle_word_list = []
+    # Make the words into list, then get all even or odd list members
+    index = 0
+    while True:
+        # If odd, append corresponding element from first word
+        if is_even_num(index):
+            print plain_word_list[index]
+            print index
+            shuffle_word_list.append(plain_word_list[index])
+        # Otherwise, append the 'even' element from the other list    
+        else:
+            print search_word_list[index]
+            print index
+            shuffle_word_list.append(search_word_list[index])    
+        # Increment the index    
+        index += 1
+    # Print out the garbage we have created.    
+    print ''.join(word_list)
+    return ''.join(word_list)
+
+def interlock(word, total_word_list):
+    for search_word in total_word_list:
+        new_word = shuffle_word(word, search_word)
+        
+    if bisect(new_word, total_word_list):
+        print new_word
+
+  
 def main():
     #create a variable called 'words_txt', make it have path of words.txt
     words_txt = swampy_dir + '/words.txt'
