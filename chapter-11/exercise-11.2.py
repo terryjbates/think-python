a444d4f4e48e66e1776d3c0e0e15f2a0cc9c21b1
Terry Bates <terryjbates@gmail.com>
2013-01-08 00:33:45 -0500

    Removed the original histogram function

--- chapter-11/exercise-11.2.py
+++ chapter-11/exercise-11.2.py
@@ -9,25 +9,12 @@ Created by Terry Bates on 2012-10-31.
 Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
 """
 
-
-def histogram(s):
-    d = dict()
-    for c in s:
-        if c not in d:
-            d[c] = 1
-        else:
-            d[c] += 1
-
-    return d
-
 def histogram(s):
     d = dict()
     for c in s:
         dict_value = d.get(c,0)
         d[c] = dict_value + 1 
     print d
-
-
 
 def main():
     histogram('h')
