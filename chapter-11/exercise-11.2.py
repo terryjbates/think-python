cf973bb325d2a4422b160e5b88694a6310204d4a
Terry Bates <terryjbates@gmail.com>
2013-01-07 22:43:02 -0500

    Created, after much exasperation, a more concise histogram algorithm, using the .get method.

---
+++ chapter-11/exercise-11.2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-11.2.py
+
+Write a more concise version of histogram function.
+
+Created by Terry Bates on 2012-10-31.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+
+    return d
+
+def histogram(s):
+    d = dict()
+    for c in s:
+        dict_value = d.get(c,0)
+        d[c] = dict_value + 1 
+    print d
+
+
+
+def main():
+    histogram('h')
+
+if __name__ =='__main__':
+    main()
