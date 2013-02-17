a6fb3dc9854873cb75815cf8885245131d2044b7
Terry Bates <terryjbates@gmail.com>
2013-02-17 04:09:16 -0500

    Created 'total_word_count' as a global var. Created 'count_word' function to count words different ways depending on if we are using a dict or list. Added new 'data_struct' positional argument. Set 'complete_word_set' to be either be a list or dict. Created 'half-half.txt' file to demonstrate performance difference between dict or list.

--- chapter-13/exercise-13.2.py
+++ chapter-13/exercise-13.2.py
@@ -16,53 +16,80 @@ import string
 import collections
 import cPickle
 
+# Declare global total word count.
+total_word_count = 0
 
-# Neat idea. Rather than using a list and "in" to see if word is valid
-# Create a dictionary with each value being = to a ceremonial value.
-# Access to the dictionary appears to be random access, versus being 
-# serially looked up as a list appears to be. This also has side-effect
-# of actually ensuring only valid words are counted after stripping of 
-# punctuation.
-
-word_list_dict_file = open('/Users/tbates/python/Think-Python/think-python/words_dict', 'rb')
-word_list_dict = cPickle.load(word_list_dict_file)
-
+def count_word(word, complete_word_set, book_word_dict):
+    global total_word_count
+    
+    # If we are using a dictionary
+    if str(type(complete_word_set)) == "<type 'dict'>":
+        try: 
+            # See if the word is in our word list dictionary.
+            if complete_word_set[word]:
+                book_word_dict[word] = book_word_dict.setdefault(word,0) + 1
+                total_word_count += 1
+        except:
+            pass        
+    # If we are using a list.
+    elif str(type(complete_word_set)) == "<type 'list'>":
+        # Use 'in' to see if word is present in valid word list
+        if word in complete_word_set:
+            book_word_dict[word] = book_word_dict.setdefault(word,0) + 1
+            total_word_count += 1
 
 def main():
+    # Create argparse object
     parser = argparse.ArgumentParser()
-    parser.add_argument("source_file", help="The text file containing the source and target urls")
+    # Use a positional argument
+    parser.add_argument("source_file", help="The text file containing the source and target urls.")
+    parser.add_argument("data_struct", help="Select 'list' or 'dict to use in checking for words.")
+    # Obtain our positional arguments
     args = parser.parse_args()
+    # Set the filename to first positional argument.
     filename = args.source_file
+    # Set the data structure type to second positional argument
+    data_struct = args.data_struct
+    
+    # First ensure that we are not picking an unspecified data structure.
+    # If we choose a list or dict, we then set 'complete_word_set' accordingly.
+    if data_struct not in ['list','dict']:
+        print "Unknown data structure '%s': please use 'dict' or 'list'" % data_struct
+        exit()
+    elif data_struct == 'list':
+        word_list_file = open('/Users/tbates/python/Think-Python/think-python/words_list', 'rb')
+        complete_word_set = cPickle.load(word_list_file)        
+    elif data_struct == 'dict':
+        word_list_dict_file = open('/Users/tbates/python/Think-Python/think-python/words_dict', 'rb')
+        complete_word_set = cPickle.load(word_list_dict_file)        
+
+    # Open a file handle for the filename passed as argument
     f = open(args.source_file,'r')
     # Create dictionary to store word-specific counts
     book_word_dict = dict()
-    # Store total word count
-    total_word_count = 0
-    # Use for loop in order to process each line.
-    # Remove all punctuation
+    
+    # We use for loop in order to process each line.
+    # Since 'string.punctuation' is literally a string,
+    # We use a loop to replace any matching characters with nothing.
     for line in f:
         for char in string.punctuation:
             line = line.replace(char,'')
         # After line is stripped of punctuation, count member words
-        #print line.lower().strip()
-        # We make all words lowercase, strip whitespace, split on spaces
-        # All in one fell swoop.
+        # we make all words lowercase, strip whitespace, split on spaces
+        # all in one fell swoop.
         for word in line.lower().strip().split():
-            try: 
-                # See if the word is in our word list dictionary.
-                if word_list_dict[word]:
-                    book_word_dict[word] = book_word_dict.setdefault(word,0) + 1
-                    total_word_count += 1
-            except:
-                pass
-    # Generate a sorted directory
+            count_word(word, complete_word_set, book_word_dict)
+            
+    # Generate a sorted directory via 'collections' module
+    # Sort it based on the count values, and reverse it,
+    # so that the largest counts appear first.
     sorted_word_dict =  collections.OrderedDict(sorted(book_word_dict.items(), reverse=True, key=lambda t: t[1]))
+
     # Print out sorted directory, keys and values
     for key in sorted_word_dict:
         pass
-        #print "%s : %s" % (key, sorted_word_dict[key])
 
-    #print book_word_dict
+    # Output our total word count, and unique word count.
     print "Total word count", total_word_count
     print "Number of unique words", len(sorted_word_dict)
 
