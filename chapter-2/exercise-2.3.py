8d4291406204a076b2e07f2a6c84a757ba70e56f
Terry Bates <terryjbates@gmail.com>
2013-02-19 01:51:38 -0500

    Removed trailing whitespace from various lines. Modified heading comment to have line length < 80 characters.

--- chapter-2/exercise-2.3.py
+++ chapter-2/exercise-2.3.py
@@ -6,7 +6,8 @@ exercise-2.3.py
 A script to print values of expressions.
 
 Created by Terry Bates on 2012-07-13.
-Copyright (c) 2012 http://the-awesome-python-blog.posterous.com. All rights reserved.
+Copyright (c) 2012 http://the-awesome-python-blog.posterous.com.
+All rights reserved.
 """
 
 import sys
@@ -17,22 +18,21 @@ def main():
     width = 17
     height = 12.0
     delimiter = '.'
-    
+
     # type should be int
     print width/2
     # type should be float
     print width/2.0
-    
+
     # type should be float
     print height/3
 
     # type should be int
     print 1 + 2 * 5
-    
+
     # type should be string
     print delimiter * 5
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
