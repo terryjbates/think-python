16f67e62feeff4496db4fc5331ecda4b03dae4a7
Terry Bates <terryjbates@gmail.com>
2013-02-24 00:47:06 -0500

    Added the patently inefficient 'random_word' function to randomly select a word from the book.

--- chapter-13/word_histogram.py
+++ chapter-13/word_histogram.py
@@ -19,10 +19,35 @@ def process_file(filename):
 def total_words(h): 
     return sum(h.values())
 
+
 def different_words(h): 
     return len(h)
-    
+
+
+def most_common(h, num=10): 
+    t = []
+    for key, value in h.items(): 
+        t.append((value, key))
+        t.sort(reverse=True) 
+    return t
+
+
+def print_most_common(hist, num=10):
+    t = most_common(hist)
+    print 'The most common words are:' 
+    for freq, word in t[0:num]:
+        print word, '\t', freq
+
+
+def random_word(h):
+    t=[]
+    for word, freq in h.items():
+        t.extend([word] * freq)
+    return random.choice(t)
+
+
 if __name__ == "__main__":
     hist = process_file('pg7178.txt')
     print 'Total number of words:', total_words(hist)
     print 'Number of different words:', different_words(hist)
+    print_most_common(hist)
