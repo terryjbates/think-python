1325888906c41a88b75377a71d06639c7abe9b00
Terry Bates <terryjbates@gmail.com>
2013-02-24 22:31:01 -0500

    Removed extraneous whitespace, made copyright line two lines to avoid running over 80 characters.

--- chapter-3/exercise-3.4.py
+++ chapter-3/exercise-3.4.py
@@ -17,18 +17,19 @@ def print_twice(my_string):
     print my_string
 
 
-def do_twice(f, num):  # this takes function object as an argument
+def do_twice(f, num):
     f(num)
     f(num)
 
 
-def do_four(f, num):  # this takes function object as an argument
+def do_four(f, num):
     do_twice(f, num)
     do_twice(f, num)
 
 
 def main():
     do_four(print_twice, 'spam')
+
 
 if __name__ == '__main__':
     main()
