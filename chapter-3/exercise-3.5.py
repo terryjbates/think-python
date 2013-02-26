b04a59b5056a207fac8931b3daa04d13eed6c9b9
Terry Bates <terryjbates@gmail.com>
2013-02-26 01:19:14 -0500

    Exercise 3.5 pep8 cleanup

--- chapter-3/exercise-3.5.py
+++ chapter-3/exercise-3.5.py
@@ -3,41 +3,51 @@
 """
 exercise-3.5.py
 
-Create a script to draw a grid as shown on page 34 of textbook "Python for Software Design"
+Create a script to draw a grid as shown on page 34 of textbook "Python for
+Software Design"
 
 Created by Terry Bates on 2012-07-15.
 Copyright (c) 2012 http://the-awesome-python-blog.posterous.com.
 All rights reserved."""
 
+
 def print_twice(my_string):
     print my_string
     print my_string
 
-def do_twice(f): # this takes function object as an argument
+
+def do_twice(f):
     f()
     f()
 
-def do_four(f): # this takes function object as an argument
+
+def do_four(f):
     do_twice(f)
     do_twice(f)
+
 
 def print_beam():
     print '+ - - - -',
-    
+
+
 def print_post():
     print'|        ',
+
 
 def print_beams():
     do_twice(print_beam)
     print '+'
 
+
 def print_posts():
     do_twice(print_post)
     print '|'
-    
+
+
 def print_row():
     print_beams()
     do_four(print_posts)
+
 
 def print_grid():
     do_twice(print_row)
@@ -47,6 +57,6 @@ def main():
     print_grid()
     print_beams()
 
-if __name__ == '__main__':
-	main()
 
+if __name__ == '__main__':
+    main()
