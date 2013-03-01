905b03eab733c5399713168324b34dd8c5148c22
Terry Bates <terryjbates@gmail.com>
2013-03-01 00:10:47 -0500

    Exercise 4.3 pep8 cleanup.

--- chapter-4/exercise-4.3.py
+++ chapter-4/exercise-4.3.py
@@ -12,14 +12,15 @@ from swampy.TurtleWorld import *
 import math
 from polygon import circle, arc
 
-def square(t,length):
+
+def square(t, length):
     for i in range(4):
-        fd(t,length)
+        fd(t, length)
         lt(t)
+
 
 def polyline(t, n, length, angle):
     """Draws n line segments.
-
     t: Turtle object
     n: number of line segments
     length: length of each segment
@@ -31,24 +32,27 @@ def polyline(t, n, length, angle):
 
 
 def polygon(t, n, length, angle):
-    for i in range(n): 
-        fd(t,length)
-        lt(t,angle) 
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
+
 
 def polygon(t, n, length):
     angle = 360.0 / n
     # repeat for as many sides(n)
     polyline(t, n, length, angle)
 
+
 def arc(t, r, angle):
     arc_length = (2 * math.pi * r) * (angle / 360)
-    n = int(arc_length /3) + 1
+    n = int(arc_length/3) + 1
     step_length = arc_length / n
-    step_angle = float (angle) / n
+    step_angle = float(angle)/n
     polyline(t, n, step_length, step_angle)
-    
-def circle(t,radius):
-    arc(t, radius, 360   )
+
+
+def circle(t, radius):
+    arc(t, radius, 360)
 
 
 def main():
@@ -58,14 +62,12 @@ def main():
     # square(bob,45)
     # square(bob,100)
     # square(bob,180)
-    
     # polygon(bob,15,30)
     # square(bob,60)
-    circle(bob,60)
+    circle(bob, 60)
     #arc(bob,80,180)
     polyline
     wait_for_user()
 
 if __name__ == '__main__':
     main()
-
