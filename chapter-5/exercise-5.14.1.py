ddf633aa8f5a8d12ba2e9b93cba27122c4d19dd5
Terry Bates <terryjbates@gmail.com>
2013-05-04 21:53:47 -0400

    Exercise 5.14.1 pep8 cleanup.

--- chapter-5/exercise-5.14.1.py
+++ chapter-5/exercise-5.14.1.py
@@ -11,14 +11,15 @@ import sys
 import os
 import math
 
-def check_fermat(a,b,c,n):
+
+def check_fermat(a, b, c, n):
     '''This function does the actual checking of Fermat's last theorem.
-    There should be no such integers such that 
-    a^n + b^n = c^n 
+    There should be no such integers such that
+    a^n + b^n = c^n
     for n values greater than 2.
     '''
-    left_side = pow(a,n) + pow(b,n)
-    right_side = pow(c,n)
+    left_side = pow(a, n) + pow(b, n)
+    right_side = pow(c, n)
     if (n > 2) and (left_side == right_side):
         print "Holy Smokes, Fermat was wrong!"
     else:
@@ -31,21 +32,19 @@ def prompt_user():
     Use a for loop to avoid repeated prompts
     '''
     # create general prompt
-    prompt = "Please enter " 
-    
+    prompt = "Please enter "
     for value in ['a', 'b', 'c', 'n']:
-        new_prompt = prompt + value + '\n' # modify prompt for value in loop
-        my_input = raw_input(new_prompt)   # get input
-        float_input = float(my_input)      # convert the entered value to float
+        new_prompt = prompt + value + '\n'  # modify prompt for value in loop
+        my_input = raw_input(new_prompt)  # get input
+        float_input = float(my_input)  # convert the entered value to float
         exec ("%s=%f" % (value, float_input))
-
     # call check_fermat function
-    check_fermat(a,b,c,n)
-    
+    check_fermat(a, b, c, n)
+
+
 def main():
     prompt_user()
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
