ff235fc4d33fd019271bf798653773ab42d4aa58
Terry Bates <terryjbates@gmail.com>
2014-07-05 18:30:10 -0700

    Created solution to exercise 16.1. Create a function called print_time to print out Time object contents colon separated.

---
+++ chapter-16/exercise-16.1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exercise-16.1.py
+
+Created by Terry Bates on 2014-07-05.
+Copyright (c) 2014 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+import sys
+import os
+
+
+class Time(object):
+    """Represents the time of day.
+
+        attributes: hour, minute, second
+    """
+
+
+def print_time(time_obj):
+    hour, minute, second = time_obj.hour, time_obj.minute, time_obj.second
+    # Use join to quickly print out string colon separate
+    print ':'.join([str(hour), str(minute), str(second)])
+    
+def main():
+    my_time = Time()
+    my_time = Time()
+    my_time.hour = 11
+    my_time.minute = 59
+    my_time.second = 30
+    print_time(my_time)
+
+if __name__ == '__main__':
+    main()
+
