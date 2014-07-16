d1df042118037cff6159a60edd98313d26a06058
Terry Bates <terryjbates@gmail.com>
2014-07-15 23:09:00 -0700

    Made recommended PEP8 modifications, adding one line between class methods, removing redundant parens.

--- chapter-17/exercise-17.4.py
+++ chapter-17/exercise-17.4.py
@@ -14,10 +14,12 @@ class Point(object):
     def __init__(self, x=None, y=None):
         self.x = x
         self.y = y
+
     def __str__(self):
         return '{0}, {1}'.format(self.x, self.y)
+
     def __add__(self, other):
-        return (self.x + other.x, self.y + other.y)
+        return self.x + other.x, self.y + other.y
 
 
 p1 = Point(5, 3)
