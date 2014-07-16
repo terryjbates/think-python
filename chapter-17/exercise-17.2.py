35fc9fbb0ea8d3c5efa5dd658af8c24375e9ceb3
Terry Bates <terryjbates@gmail.com>
2014-07-15 23:45:36 -0700

    Created __add__ method to Point class. Will add either a Point object or tuple to a Point.

--- chapter-17/exercise-17.2.py
+++ chapter-17/exercise-17.2.py
@@ -14,5 +14,16 @@ class Point(object):
     def __init__(self, x=None, y=None):
         self.x = x
         self.y = y
+
     def __str__(self):
         return '{0}, {1}'.format(self.x, self.y)
+
+    def __add__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x + other.x, self.y + other.y)
+        if isinstance(other, tuple):
+            # We get first and second items of tuple
+            other_x = tuple[0]
+            other_y = tuple[1]
+            # Add the values to the self Point and return new point
+            return Point(self.x + other_x, self.x + other_y)
